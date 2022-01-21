9fcda7b56aa05ae108d906eea06316bae3b19f07
Saivior1337 <91725222+Saivior1337@users.noreply.github.com>
2022-01-21 23:52:12 +0300

    Update Math.h

--- Math.h
+++ Math.h
@@ -9,8 +9,6 @@ public:
 	D3DXVECTOR3 CalcAngle(register const D3DXVECTOR3& src, register const D3DXVECTOR3& dst, float* punch);
 	float GetFOV(float* angle, float* src, float* dst);
 	D3DXVECTOR3 ClampAngle(D3DXVECTOR3 qaAng);
-	bool ScreenTransform(const D3DXVECTOR3& point, D3DXVECTOR3& screen);
-	bool WorldToScreen(const D3DXVECTOR3& origin, D3DXVECTOR3& screen, int w, int h);
 };
 
 extern Math * iMath;
